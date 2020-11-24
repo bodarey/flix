445bb4e5a62004a50aaee758a83e24f2b7308266
bodarey <bodarey@yahoo.com>
2020-11-24 16:29:46 +0200

    master check photo1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 config.force_ssl = true
+config.serve_static_assets = true
+config.assets.compile = true
+
+
 end
